df315f68f82481fab63359620b1ee4f0bb74c15e
Giovannitk <tassi.giovanni@outlook.it>
2024-10-22 01:06:31 +0200

    Added of deleting the users just by role ADMIN

--- javaSpring-DB_H2-backend/task-manager/src/main/java/com/example/taskmanager/task_manager/controller/UserController.java
+++ javaSpring-DB_H2-backend/task-manager/src/main/java/com/example/taskmanager/task_manager/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.taskmanager.task_manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +47,18 @@ public class UserController {
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed! Incorrect username or password.");
         }
+    }
+
+    @DeleteMapping("/users/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+    	try {
+    		userDetailsService.deleteAllTasksByUserId(id);
+    		userDetailsService.deleteUserById(id);
+            return ResponseEntity.ok("User deleted successfully");
+    	}catch(EmptyResultDataAccessException ex) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("id=" + id + " -->" + ex.getMessage());
+    	}
     }
 
 }
